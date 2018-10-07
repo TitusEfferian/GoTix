4b5fcc57014d5155dc6de27cc074a4e55165f2f2
titusefferian <kiritosato@gmail.com>
2018-10-07 22:29:43 +0700

    eventcard

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import {
     View,
-    StyleSheet
+    StyleSheet,
+    Image
 } from 'react-native'
 
-const EventCard = (props)=>{
-    return(
-        <View>
-
+const EventCard = (props) => {
+    return (
+        <View style={styles.mainContainer}>
+            <Image style={styles.imageStyle}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    mainContainer: {
+        flex: 1,
+        borderRadius: 8,
+    },
+    imageStyle:{
+        flex:1,
+        height:256,
+    }
 })
 
 export default EventCard
