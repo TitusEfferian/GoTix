323425e49902206a8a1ecad219c65d04086aca8c
titusefferian <kiritosato@gmail.com>
2018-10-14 22:33:06 +0700

    search

--- src/activity/Search.js
+++ src/activity/Search.js
@@ -5,52 +5,70 @@ import {
   TextInput,
   Platform,
   TouchableOpacity,
-  ScrollView
+  ScrollView,
+  Text
 } from 'react-native'
 import { connect } from 'react-redux'
 import { requestSearch } from '../redux/actions/search'
 import { MAIN_CONTAINER, SECONDARY_COLOR } from '../style/style';
 import Icon from 'react-native-vector-icons/Ionicons'
-import {isEqual} from 'lodash'
+import { isEqual } from 'lodash'
+import MovieContainer from '../components/SearchMovieorEvents'
+import Loading from '../components/Spinner'
 class Search extends React.PureComponent {
   static navigationOptions = {
     header: null
   }
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      inputText:null
+    this.state = {
+      inputText: null
     }
   }
-
-  
 
   render() {
     console.log(this.props.search)
     return (
       <View style={styles.mainContainer}>
-        <ScrollView>
-          <View style={{
-            backgroundColor: SECONDARY_COLOR,
-            flex: 1,
-            flexDirection: 'row',
-            alignItems: 'center',
-            paddingVertical: 8,
-            paddingLeft: 8,
-            marginTop: Platform.OS == 'ios' ? 24 : 0
-          }}>
-            <TouchableOpacity onPress={() => { this.props.navigation.goBack() }}>
-              <Icon style={{ marginRight: 16 }} name={Platform.OS == 'ios' ? 'ios-arrow-back' : 'md-arrow-back'} color={'white'} size={24} />
-            </TouchableOpacity>
-            <TextInput
-              style={{ flex: 1, padding: 8,backgroundColor:'white',borderRadius:8,marginRight:8 }}
-              placeholder={'Search Movies or Events'}
-              onChangeText={(text)=>{this.setState({inputText:text}),this.props.searchRequest(text)}}
-              value={this.state.text}
-            />
-          </View>
+        <View style={{
+          backgroundColor: SECONDARY_COLOR,
+          flexDirection: 'row',
+          alignItems: 'center',
+          paddingVertical: 8,
+          paddingLeft: 8,
+          marginTop: Platform.OS == 'ios' ? 24 : 0
+        }}>
+          <TouchableOpacity onPress={() => { this.props.navigation.goBack() }}>
+            <Icon style={{ marginRight: 16 }} name={Platform.OS == 'ios' ? 'ios-arrow-back' : 'md-arrow-back'} color={'white'} size={24} />
+          </TouchableOpacity>
+          <TextInput
+            style={{ flex: 1, padding: 8, backgroundColor: 'white', borderRadius: 8, marginRight: 8 }}
+            placeholder={'Search Movies or Events'}
+            onChangeText={(text) => { this.setState({ inputText: text }), text.length>0 ? this.props.searchRequest(text) : null}}
+            value={this.state.text}
+          />
+        </View>
+        <ScrollView style={styles.mainContainer}>
+          {
+            this.state.inputText != null
+              ?
+              this.props.search.data != undefined && !this.props.search.isFetching
+                ?
+                <MovieContainer
+                  thumbnail={this.props.search.data.data.movies[0].thumbnail}
+                  name={this.props.search.data.data.movies[0].name}
+                  tags={this.props.search.data.data.movies[0].tags}
+                />
+                :
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 32 }}>
+                  <Loading />
+                </View>
+              :
+              null
+          }
         </ScrollView>
       </View>
+
     )
   }
 }
---
+++ src/components/SearchMovieorEvents.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet
+} from 'react-native'
+import Loading from './Spinner'
+import { WHITE, DEFAUL_TEXT_STYLE_BOLD, DEFAULT_TEXT_STYLE, SECONDARY_COLOR, TEXT_BLUE_COLOR } from '../style/style';
+class MovieCardBox extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      width: null,
+      height: null,
+      loading: false
+    }
+  }
+  componentDidMount() {
+    Image.getSize(this.props.thumbnail, (x, y) => {
+      this.setState({
+        width: x,
+        height: y
+      })
+    })
+  }
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <Image
+          resizeMode='stretch'
+          source={{ uri: this.props.thumbnail }}
+          style={{ width: this.state.width / 4, height: this.state.height / 4 ,marginRight:16}}
+        />
+        <View style={{flex:1}}>
+          <Text style={[DEFAUL_TEXT_STYLE_BOLD]} numberOfLines={1}>{this.props.name}</Text>
+          <Text style={[DEFAULT_TEXT_STYLE,{marginTop:16,color:TEXT_BLUE_COLOR}]}><Text style={[DEFAULT_TEXT_STYLE,{color:SECONDARY_COLOR}]}>Movie</Text> | {this.props.tags}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 16,
+  }
+})
+export default MovieCardBox
